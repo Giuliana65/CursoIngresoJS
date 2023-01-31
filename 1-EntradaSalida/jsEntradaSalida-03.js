1c280c76d28b0311042c6447e72dabe831d14f45
Giuliana <giulianabarbalarga@gmail.com>
2023-01-31 00:21:14 -0300

    Ultimos cambios

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,13 @@
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+ var nombre ;
+
+ nombre = document.getElementById("nombre").value;
+
+ alert(nombre);
 
 }
-
-
